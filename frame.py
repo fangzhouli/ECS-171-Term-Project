d00f4fb78ff1595279f58c61a1629601faae17d0
fangzhouli <fzli0805@gmail.com>
2018-11-18 22:00:39 -0800

    improved to process all files.

--- frame.py
+++ frame.py
@@ -4,6 +4,7 @@ Frame play-by-play row data files.
 Usage:
 	Change path1 to be the path of your event file, and path2 to be that of players file.
 '''
+root = '/Users/fzli/Desktop/ECS/171/Project/Basketball_data/PlayByPlay_'
 
 path1 = '/Users/fzli/Desktop/ECS/171/Project/' + \
 		'Basketball_data/PlayByPlay_2010/Events_2010.csv'
@@ -23,29 +24,40 @@ import os
 import csv
 
 def makeDataCsv():
-	curr = 1
+	curr = 0
 	process = 0
 
 	print("Start reading CSV files...")
-	events = readFile(path1)
-	players = readFile(path2)
+	events = []
+	players = []
+	for i in range(8):
+		year = 2010 + i
+		print("File " + str(year) + '...')
+		path = root + str(year)
+		filepath = path + '/Events_' + str(year) + '.csv'
+		events.extend(readFile(filepath))
+		filepath = path + '/Players_' + str(year) + '.csv'
+		players.extend(readFile(filepath))
+
+	offset = int(players[0][0])
+	divisor = round(len(events) / 100)
 	# Initialize empty cells with the first column is player ID
 	lines = []
-	for elm in players[1:]:
-		line = [elm[0], '2010']
+	for elm in players:
+		line = [elm[0], elm[1]]
 		line.extend([0] * 26)
 		lines.append(line)
 	# Store player ID and initial time of who are playing on the field
 	print("Start processing event file...")
 	playerID = []
 	start = []
-	for event in events[1:]:
+	for event in events:
 		# Print complete ratio of the process
-		if not round(curr / 25000) == process:
-			process = round(curr / 25000)
+		if not round(curr / divisor) == process:
+			process = round(curr / divisor)
 			print('Process complete: ', process, '%')
 		# If event type is sub_in, record
-		row = int(event[9]) - 600001
+		row = int(event[9]) - offset
 		if event[10] == 'sub_in':
 			playerID.append(event[9])
 			start.append(int(event[7]))
@@ -68,14 +80,14 @@ def makeDataCsv():
 		# Check if it is the last event for a match. Empty playerID and start if it is
 		if curr + 1 == len(events) or int(event[7]) > int(events[curr + 1][7]):
 			while playerID:
-				lines[int(playerID[0]) - 600001][2] += max(2400, int(event[7])) - start[0]
+				lines[int(playerID[0]) - offset][2] += max(2400, int(event[7])) - start[0]
 				playerID.pop(0)
 				start.pop(0)
 		curr += 1
 
 	# Write to a new csv file
-	if not os.path.isfile('2010_data.csv'):
-		with open('2010_data.csv', 'w') as outcsv:
+	if not os.path.isfile('data.csv'):
+		with open('data.csv', 'w') as outcsv:
 			writer = csv.writer(outcsv)
 			writer.writerow(header)
 			writer.writerows(lines)
