0cd522b713b499aa0f2c30876e0f87d8af071b0a
Timothy Zhang <tsme4wk@gmail.com>
2018-11-20 00:13:39 -0800

    added generated dataset to test 'predict_from_model()'

--- mlp/fake_feature/genFake.R
+++ mlp/fake_feature/genFake.R
@@ -1,12 +1,13 @@
 # generate fake data as placeholder for feature.csv
 library(data.table) # install.packages('data.table')
 set.seed(1234)
+# For testing code related to model training
 # 10000 samples x sampeID+(10 features)+result
 n_samp <- 10000
 dt <- data.table(matrix(nrow=n_samp, ncol = 12))
 colnames(dt) <- c('sampleID', paste0('a_f', 1:5), paste0('b_f', 1:5), 'win')
 
-dt[, 'sampleID'] <- 1:10000
+dt[, 'sampleID'] <- 1:n_samp
 dt[, 'a_f1'] <- round(rnorm(n_samp, 10000, 800))
 dt[, 'b_f1'] <- round(rnorm(n_samp, 10000, 800))
 dt[, 'a_f2'] <- round(rnorm(n_samp, 9000, 200))
@@ -22,4 +23,28 @@ dt[, win := as.numeric((0.5*(a_f1-b_f1) + 0.3*(a_f2-b_f2) - 0.6*(a_f3-b_f3)
                         + 0.4*(a_f4-b_f4) - 0.4*(a_f5-b_f5)
                         + runif(n_samp, -250, 250)) >= 0)]
 
-write.csv(dt, file = 'feature.csv', row.names=F)
+write.csv(dt[1:10000, ], file = 'feature.csv', row.names=F)
+
+# For testing code related to 'load_model()'
+dt <- data.table(matrix(nrow=n_samp, ncol = 12))
+colnames(dt) <- c('sampleID', paste0('a_f', 1:5), paste0('b_f', 1:5), 'win')
+
+dt[, 'sampleID'] <- 1:n_samp
+dt[, 'a_f1'] <- round(rnorm(n_samp, 10000, 800))
+dt[, 'b_f1'] <- round(rnorm(n_samp, 10000, 800))
+dt[, 'a_f2'] <- round(rnorm(n_samp, 9000, 200))
+dt[, 'b_f2'] <- round(rnorm(n_samp, 9000, 200))
+dt[, 'a_f3'] <- round(rnorm(n_samp, 6000, 120))
+dt[, 'b_f3'] <- round(rnorm(n_samp, 6000, 120))
+dt[, 'a_f4'] <- round(rnorm(n_samp, 4000, 150))
+dt[, 'b_f4'] <- round(rnorm(n_samp, 4000, 150))
+dt[, 'a_f5'] <- round(rnorm(n_samp, 1000, 210))
+dt[, 'b_f5'] <- round(rnorm(n_samp, 1000, 210))
+
+dt[, win := as.numeric((0.5*(a_f1-b_f1) + 0.3*(a_f2-b_f2) - 0.6*(a_f3-b_f3)
+                        + 0.4*(a_f4-b_f4) - 0.4*(a_f5-b_f5)
+                        + runif(n_samp, -250, 250)) >= 0)]
+
+write.csv(dt[1:n_samp, ], file = 'input_with_wincol.csv', row.names=F)
+write.csv(dt[1:n_samp, -ncol(dt), with=F],
+          file = 'input_no_wincol.csv', row.names=F)
