ba55803b6cc0da70db3e9cd8d352823e8d62b4ca
Timothy Zhang <tsme4wk@gmail.com>
2018-11-26 23:30:13 -0800

    Added report directory with combined NCAA images and combiner script

---
+++ report/scripts/NCAA_combiner.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import os
+
+
+def combine(type):
+    """ Combine plots in /mlp/NCAA/plots/learningRate10e-1
+        Input:
+          - @type: either 'accuracy' or 'weights'
+    """
+    rootdir = './mlp/NCAA/plots/learningRate10e-1'
+    files = []
+    for dir, subdirs, fs in os.walk(rootdir):
+        for f in fs:
+            if f.split('_')[-1] == type + '.png':
+                files += [os.path.join(dir,f)]
+    fs.sort()
+    fs.remove(fs[0]) # remove NCAA_0_1_compact*
+
+    # Coordinates
+    x = 0
+    y = 0
+    # Output canvas
+    out = Image.new("RGB", (2400, 3600))
+
+    for i in range(len(files)):
+        img = Image.open(files[i])
+        img = img.resize([800, 600], Image.ANTIALIAS)
+        if i % 6 == 0 and i != 0:
+            x+=800
+        y += 600
+        if i % 6 == 0:
+            y=0
+        w, h = img.size
+        out.paste(img, (x, y, x+w, y+h))
+
+    out.save('./report/images/NCAA_18_' + type + '.jpg')
