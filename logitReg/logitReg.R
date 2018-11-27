d6b7aeb0bf4c95aeaba0cd220eae66c3fe2e1f1b
Timothy Zhang <tsme4wk@gmail.com>
2018-11-27 15:26:41 -0800

    added executable lines to run pre&post game datasets automatically

--- logitReg/logitReg.R
+++ logitReg/logitReg.R
@@ -4,15 +4,23 @@ set.seed(1234)
 
 # Run logistic regression on 'features.csv'
 # Input:
+#   - path: str,
+#       path to the dataset.
 #   - n_train: int, default 40113
 #       Number of rows from the beginning to be used for training. Default is
 #       40113 because 2018 matches start from 40114.
 #   - random: boolean, default FALSE
 #       Flag to indicate program to randomly choose training samples.
-run <- function(n_train=40113, random=FALSE)
+run <- function(path, n_train, random)
 {
+  options(warn=-1)
+  cat(paste0("Running ", path, '\n'))
   # Remove index column
-  dt <- fread('features.csv', drop=1)
+  dt <- fread(path, drop=1)
+
+  ns <- colnames(dt)
+  ns[length(ns)] <- 'win'
+  colnames(dt) <- ns
 
   if(random)
     trainIdx <- sample(nrow(dt), n_train)
@@ -38,3 +46,7 @@ run <- function(n_train=40113, random=FALSE)
   cat(paste0("Training Accuracy:\t", round(acc_train, 4), '\n'))
   cat(paste0("Testing Accuracy:\t", round(acc_test, 4), '\n'))
 }
+
+run('./data_processing/output/post_game_team_diff.csv',
+    n_train=27524, random=T)
+run('./pre_game_teams.csv', n_train=40113, random=F)
